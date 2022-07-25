a3188df49ae957e21aa8320a884b2f442e9ce169
Ali Sir Aydemir <72253887+alisiraydemir@users.noreply.github.com>
2022-07-25 20:06:49 +0300

    Update trendyolpro.py

--- trendyolproject/trendyolproject/spiders/trendyolpro.py
+++ trendyolproject/trendyolproject/spiders/trendyolpro.py
@@ -6,7 +6,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.chrome.service import Service
-import time
+
 
 
 
@@ -14,14 +14,20 @@ driver = webdriver.Chrome(executable_path=r'C:/Web_driver_for_selenium/chromedri
 
 driver.get('https://www.trendyol.com/')
 
+#elementler tıklanabilir olana kadar bekle sonra xpath'ini verdiğim butona tıkla.
 kabul_et =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="onetrust-accept-btn-handler"]'))).click()
-
+# arama kutusuna erişim için.
 arama =  WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//input[@placeholder="Aradığınız ürün, kategori veya markayı yazınız"]')))
+# arama kutusunda yazılı bir şey varsa temizler.
 arama.clear()
+# verisini çekmek istediğiniz kategoriyi ya da ürünü yazınız.
 anahtarkelime = input('Lütfen aramak istediğiniz öğeyi yazınız: ')
+# arama kutusuna anahtarkelimeyi gönderir.
 arama.send_keys(anahtarkelime)
+# arama yapabilmek için enter'a 'basar'.
 arama.send_keys(Keys.ENTER)
 
+# scrapy'nin kullanabilmesi için gerekli url. (aramayı yaptığınız sayfanın url'i)
 current_link = str(driver.current_url)
 
 
